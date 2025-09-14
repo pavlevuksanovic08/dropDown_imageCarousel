a833a4f038e28cd3323c69ca16ae9b053e198b33
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2025-09-14 13:51:28 +0200

    DropDown

---
+++ src/dropDown.js
@@ -0,0 +1,26 @@
+
+export class DropDownMenu {
+    constructor(button, div) {
+        this.button = button;
+        this.div = div;
+
+        this.div.style.display = "none";
+
+        this.#handle();
+    }
+
+    #handle() {
+        this.button.addEventListener("mouseenter", () => {
+            this.div.style.display = "block";
+        })
+        this.button.addEventListener("mouseleave", () => {
+            this.div.style.display = "none";
+        })
+        this.div.addEventListener("mouseenter", () => {
+            this.div.style.display = "block";
+        })
+        this.div.addEventListener("mouseleave", () => {
+            this.div.style.display = "none";
+        })
+    }
+}
